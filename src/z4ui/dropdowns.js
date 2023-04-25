ce2f3585ec28813dedbab3a02d1a76e545e9b6d5
dunsparce9 <66077935+dunsparce9@users.noreply.github.com>
2023-04-25 18:42:47 +0300

    Change dropdown menus on hover

--- src/z4ui/dropdowns.js
+++ src/z4ui/dropdowns.js
@@ -1,22 +1,34 @@
+var dropdownOpened = false;
 $(document).ready(function () {
   $(".dropdown-button").click(function () {
-    const currentState = this.nextElementSibling.style.display;
-    hideDropdowns();
-    if (!(currentState === "flex")) {
-      this.nextElementSibling.style.display = "flex";
-      this.classList.add("opened");
-    }
+    showMenu(this);
   });
-  $(document).click(function (e) {
-    if (
-      !e.target.closest(".dropdown-menu") &&
-      !e.target.closest(".dropdown-button")
-    ) {
-      hideDropdowns();
+
+  $(".dropdown-button").mouseenter(function () {
+    if (dropdownOpened) {
+      showMenu(this);
     }
   });
 });
+$(document).click(function (e) {
+  if (
+    !e.target.closest(".dropdown-menu") &&
+    !e.target.closest(".dropdown-button")
+  ) {
+    hideDropdowns();
+  }
+});
+function showMenu(dropdown) {
+  const currentState = dropdown.nextElementSibling.style.display;
+  hideDropdowns();
+  if (!(currentState === "flex")) {
+    dropdown.nextElementSibling.style.display = "flex";
+    dropdown.classList.add("opened");
+  }
+  dropdownOpened = true;
+}
 export default function hideDropdowns() {
+  dropdownOpened = false;
   document.querySelectorAll(".dropdown-button").forEach((button) => {
     button.classList.remove("opened");
     button.nextElementSibling.style.display = "none";
