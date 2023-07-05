42ec606dd9025b9e49ac9c7daf459061043e2c06
dunsparce9 <66077935+dunsparce9@users.noreply.github.com>
2023-07-05 21:00:13 +0300

    Try to add back icons

--- src/z4ui/dropdowns.js
+++ src/z4ui/dropdowns.js
@@ -1,4 +1,11 @@
 var dropdownOpened = false;
+export default function hideDropdowns() {
+  dropdownOpened = false;
+  document.querySelectorAll(".dropdown-button").forEach((button) => {
+    button.classList.remove("opened");
+    if (button.firstElementChild) button.firstElementChild.style.display = "none";
+  });
+}
 $(document).ready(function () {
   $(".dropdown-button").click(function () {
     showMenu(this);
@@ -8,6 +15,9 @@ $(document).ready(function () {
     if (dropdownOpened && !this.classList.contains('opened')) {
       showMenu(this);
     }
+  });
+  $(".dropdown-menu > li").click(function (e) {
+    hideDropdowns();
   });
 });
 $(document).click(function (e) {
@@ -19,18 +29,11 @@ $(document).click(function (e) {
   }
 });
 function showMenu(dropdown) {
-  const currentState = dropdown.nextElementSibling.style.display;
+  const currentState = dropdown.firstElementChild.style.display;
   hideDropdowns();
   if (!(currentState === "flex")) {
-    dropdown.nextElementSibling.style.display = "flex";
+    dropdown.firstElementChild.style.display = "flex";
     dropdown.classList.add("opened");
   }
   dropdownOpened = true;
-}
-export default function hideDropdowns() {
-  dropdownOpened = false;
-  document.querySelectorAll(".dropdown-button").forEach((button) => {
-    button.classList.remove("opened");
-    button.nextElementSibling.style.display = "none";
-  });
 }
